519ffef75ba1f17092560cb8badb69aec5a8e6dd
Crispin R <crispinr003@gmail.com>
2021-10-10 12:32:54 +0530

    Made 404 look better

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,73 @@
 import React from "react";
+import Particles from "react-particles-js";
 
 export default function error() {
-  return <div></div>;
+  return (
+    <>
+      <section>
+        <Particles
+          className="partic"
+          params={{
+            particles: {
+              number: {
+                value: 160,
+                density: {
+                  enable: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  speed: 4,
+                  size_min: 0.3,
+                },
+              },
+              line_linked: {
+                enable: false,
+              },
+              move: {
+                random: true,
+                speed: 1,
+                direction: "top",
+                out_mode: "out",
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "bubble",
+                },
+                onclick: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+              modes: {
+                bubble: {
+                  distance: 250,
+                  duration: 2,
+                  size: 0,
+                  opacity: 0,
+                },
+                repulse: {
+                  distance: 400,
+                  duration: 4,
+                },
+              },
+            },
+          }}
+        />
+      </section>
+      <section className="position-absolute top-50 start-50 translate-middle">
+        <h2 className="text-center">Oops, page not found!</h2>
+        <h4 className="text-center">
+          <a href="/" className="text-decoration-none txt-cyan txt-hover">
+            Need help?
+          </a>
+        </h4>
+      </section>
+    </>
+  );
 }
