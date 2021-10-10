6ce4afa88abbdbbf0bf0a2f32160c766ea602105
Crispin R <crispinr003@gmail.com>
2021-10-10 23:29:31 +0530

    Add load route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import "./assets/scss/style.scss";
 import home from "./pages/home";
 import error from "./pages/404";
+import loader from "./components/loader";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={home} />
+          <Route exact path="/load" component={loader} />
           <Route exact path="/404" component={error} />
           <Redirect to="/404" />
         </Switch>
