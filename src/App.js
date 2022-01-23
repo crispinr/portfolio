b91d5186f80d4021ce6a6bb4b8ed192303c7cfdb
Crispin R <crispinr003@gmail.com>
2022-01-23 17:39:39 +0530

    Remove loader route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
 import "./assets/scss/style.scss";
 import home from "./pages/home";
 import error from "./pages/404";
-import loader from "./components/loader";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={home} />
-          <Route exact path="/load" component={loader} />
           <Route exact path="/404" component={error} />
           <Redirect to="/404" />
         </Switch>
