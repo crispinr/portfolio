739f538e8b61104a0492f81434c13539742985ce
Crispin R <crispinr003@gmail.com>
2022-02-01 01:17:16 +0530

    Add about me section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Bonfire from "../assets/images/Bonfire.jpg";
 
 export default function home() {
   return (
@@ -18,6 +19,21 @@ export default function home() {
             </h6>
           </div>
           <hr className="w-25 lblue-bg" />
+        </div>
+      </section>
+      <section
+        id="about"
+        className="container d-flex flex-column justify-content-center align-items-center"
+      >
+        <h3 className="cyan-txt mt-5">About Me</h3>
+        <div className="d-flex justify-content-center align-items-center">
+          <img src={Bonfire} alt="bonfire" className="img-card" />
+          <p className="ms-5 w-50">
+            A sophomore who loves to develop cool stuff and likes facts, art
+            &amp; crafts, space, music, and &lt;code /&gt;. I'm currently
+            pursuing my Bachelor of Technology in Information Technology at
+            Loyola - ICAM College of Engineering and Technology.
+          </p>
         </div>
       </section>
       <div className="vh-100">cool</div>
