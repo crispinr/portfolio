b580902078de768faab7cd743a7ae547af62fce2
Crispin R <crispinr003@gmail.com>
2022-02-01 22:26:26 +0530

    Made hero responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,16 +16,14 @@ export default function home() {
     <>
       <NavBar />
       <section id="hero">
-        <div className="container w-50 text-center d-flex flex-column justify-content-center align-items-center vh-100">
-          <div className="d-flex flex-column justify-content-center align-items-center h-50 w-100 mt-5">
+        <div className="container col-md-6 mx-auto text-center d-flex flex-column justify-content-center align-items-center vh-100">
+          <div className="d-flex flex-column justify-content-center align-items-center h-50 w-100 mt-md-5 mt-0">
             <h3 className="display-4 my-5">Hola, Universe!</h3>
             <h6 className="fs-3 lblue-txt">
               “I am so clever that sometimes I don't understand a single word of
               what I am saying.”
             </h6>
-            <h6 className="align-self-end w-25 mt-3 fs-5 lblue-txt">
-              - Oscar Wilde
-            </h6>
+            <h6 className="w-md-25 mt-3 fs-5 lblue-txt">- Oscar Wilde</h6>
           </div>
           <hr className="w-25 lblue-bg" />
         </div>
