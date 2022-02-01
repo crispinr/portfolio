d1e2bcf08ff3d6a18885b8a00aa71dc2ad720c6f
Crispin R <crispinr003@gmail.com>
2022-02-01 20:54:15 +0530

    Add margin to hero

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,7 +17,7 @@ export default function home() {
       <NavBar />
       <section id="hero">
         <div className="container w-50 text-center d-flex flex-column justify-content-center align-items-center vh-100">
-          <div className="d-flex flex-column justify-content-center align-items-center h-50 w-100">
+          <div className="d-flex flex-column justify-content-center align-items-center h-50 w-100 mt-5">
             <h3 className="display-4 my-5">Hola, Universe!</h3>
             <h6 className="fs-3 lblue-txt">
               “I am so clever that sometimes I don't understand a single word of
