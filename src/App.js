d4e1b1146f33779736e7146daee502d6f1bb7d91
Crispin R <crispinr003@gmail.com>
2022-02-02 09:42:20 +0530

    Capitalize route name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/scss/style.scss";
-import home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={home} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </Router>
     </>
